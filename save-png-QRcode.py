69c37857e36268a9b6d4c8c0406378b6b8a9be1f
v-xuhaitian <v-xuhaitian@xiaomi.com>
2020-04-28 18:36:51 +0800

    add  screenshot

---
+++ save-png-QRcode.py
@@ -0,0 +1,28 @@
+import selenium
+from  selenium import webdriver
+import time
+import os
+
+w = webdriver.Chrome()
+w.get("http://www.baidu.com")
+w.implicitly_wait(10)
+w.maximize_window()
+ipt = w.find_element_by_id("kw")
+ipt.send_keys("1111")
+time.sleep(1)
+# w.get_screenshot_as_file(u"../image/test.png")
+filename =  "D:\\testclass.png"
+try:
+    picture=w.get_screenshot_as_file(filename)  
+    print(picture)
+    if picture == True:        
+            print (picture,":截图成功！图片保存路径为：",filename)    
+    else:
+       print(picture,":截图失败！")
+except Exception as e:
+   print(e)
+   w.quit()
+print (os.getcwd())#获得当前工作目录
+print (os.path.abspath('.'))#获得当前工作目录
+print (os.path.abspath('..'))#获得当前工作目录的父目录
+print (os.path.abspath(os.curdir))#获得当前工作目录
--- select-alerts-scroll-window.py
+++ select-alerts-scroll-window.py
@@ -5,47 +5,47 @@ import time
 
 # 以W3C下拉菜单为例
 w  =  webdriver.Chrome()
-# w.get("https://www.w3school.com.cn/tiy/t.asp?f=html_select")
-# w.implicitly_wait(10)
-# w.switch_to.frame("iframeResult")
+w.get("https://www.w3school.com.cn/tiy/t.asp?f=html_select")
+w.implicitly_wait(10)
+w.switch_to.frame("iframeResult")
 
 
 
-# sel = w.find_element_by_tag_name("select")
-# select = Select(sel)
-# select.select_by_index(2)
-# time.sleep(1)
-# select.select_by_value("saab")
-# time.sleep(1)
-# select.select_by_visible_text("Audi")
+sel = w.find_element_by_tag_name("select")
+select = Select(sel)
+select.select_by_index(2)
+time.sleep(1)
+select.select_by_value("saab")
+time.sleep(1)
+select.select_by_visible_text("Audi")
 
-# # 以W3C的alert为例
-# w.get("https://www.w3school.com.cn/tiy/t.asp?f=hdom_alert")
-# w.switch_to.frame("iframeResult")
-# w.find_element_by_xpath("//input[@type='button']").click()
-# alert =  w.switch_to.alert
-# print(alert.text)
-# time.sleep(1)
-# alert.accept()
-# # 拒绝
-# # alert.dismiss()
-
-# # 以W3C的confim为例
-# # js='window.open("https://www.w3school.com.cn/tiy/t.asp?f=hdom_confirm");'
-# # w.execute_script(js)
-
-# w.get("https://www.w3school.com.cn/tiy/t.asp?f=hdom_confirm")
-# w.switch_to.frame("iframeResult")
-# w.find_element_by_xpath("//input[@type='button']").click()
-
-# alert = w.switch_to.alert
-# time.sleep(2)
+# 以W3C的alert为例
+w.get("https://www.w3school.com.cn/tiy/t.asp?f=hdom_alert")
+w.switch_to.frame("iframeResult")
+w.find_element_by_xpath("//input[@type='button']").click()
+alert =  w.switch_to.alert
+print(alert.text)
+time.sleep(1)
+alert.accept()
+# 拒绝
 # alert.dismiss()
-# time.sleep(2)
-# alert.accept()
 
-# js='window.open("https://www.jianshu.com/p/3aa45532e179");'
+# 以W3C的confim为例
+# js='window.open("https://www.w3school.com.cn/tiy/t.asp?f=hdom_confirm");'
 # w.execute_script(js)
+
+w.get("https://www.w3school.com.cn/tiy/t.asp?f=hdom_confirm")
+w.switch_to.frame("iframeResult")
+w.find_element_by_xpath("//input[@type='button']").click()
+
+alert = w.switch_to.alert
+time.sleep(2)
+alert.dismiss()
+time.sleep(2)
+alert.accept()
+
+js='window.open("https://www.jianshu.com/p/3aa45532e179");'
+w.execute_script(js)
 w.get("https://www.jianshu.com/p/3aa45532e179")
 time.sleep(5)
 js_scroll1="window.scrollTo(0,100)"
