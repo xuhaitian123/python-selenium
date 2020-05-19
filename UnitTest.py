682feff22b7e97b1b951d45278cc96c0d8f818c9
v-xuhaitian <v-xuhaitian@xiaomi.com>
2020-05-19 18:38:38 +0800

    add html report

--- UnitTest.py
+++ UnitTest.py
@@ -11,8 +11,16 @@ class TestUnittest(unittest.TestCase):
         self.wb.maximize_window()
         print("setUp")
     def testClick(self):
-        self.wb.find_element_by_id("kw").send_keys("1111111111111111111")
-        print("测试case")
+        self.wb.find_element_by_id("kw").send_keys("selenium")
+        self.wb.find_element_by_id("su").click()
+        time.sleep(1)
+        text = self.wb.title
+        try:
+            self.assertIn("selenium",text)
+        except AssertionError:
+            self.wb.get_screenshot_as_file("D:\\testclass.png")
+            print("测试case")
+            raise
         time.sleep(3)
     def tearDown(self):
         time.sleep(3)
